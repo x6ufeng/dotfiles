a3dfa8caa70f5b305d35bebb13a59315b45451ff
x6ufeng <freddie.hsu@outlook.com>
2026-01-04 11:56:32 +0900

    change

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -3,8 +3,8 @@
     ./tailscale.nix
     ./anki-sync.nix
     ./cloudflared.nix
-    ./calibre-web.nix
+    #    ./calibre-web.nix
     ./booklore.nix
-    ./minecraft.nix
+    #    ./minecraft.nix
   ];
 }
