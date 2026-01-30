1d93f5f8b191cef3076e805f7db2a776d821e836
x6ufeng <freddie.hsu@outlook.com>
2026-01-30 22:09:51 +0900

    change

---
+++ modules/services/audiobookshelf.nix
@@ -0,0 +1,9 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  services.audiobookshelf = {
+    enable = true;
+  };
+}
--- modules/services/default.nix
+++ modules/services/default.nix
@@ -3,6 +3,7 @@
     ./tailscale.nix
     ./anki-sync.nix
     ./cloudflared.nix
+    ./audiobookshelf.nix
     ./stirling.nix
     ./jellyfin.nix
     #    ./minecraft.nix
